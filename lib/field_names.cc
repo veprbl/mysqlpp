d8b2f41106b8676cabda2b1190ae6de16196a3e4
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2004-11-04 04:17:56 +0000

    - #include filename fix - Code style improvements
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@240 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/field_names.cc
+++ lib/field_names.cc
@@ -3,14 +3,16 @@
 
 #include "field_names.h"
 
-#include "result2.hh"
+#include "result.h"
 
-void FieldNames::init(const ResUse *res) {
+void FieldNames::init(const ResUse *res)
+{
   int num = res->num_fields();
   reserve(num);
   for (int i = 0; i < num; i++) {
-		std::string p(res->fields()[i].name); str_to_lwr(p);   push_back(p);
+		std::string p(res->fields()[i].name);
+		str_to_lwr(p);
+		push_back(p);
   }
-	
 }
 
