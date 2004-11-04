62eb46f09d58cbc4935921281db4f97ce1d22512
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2004-11-04 04:08:30 +0000

    Created to hold code that cannot be in the *.h file without creating a circular dependency among our header and one or more of the headers it includes.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@229 5946f024-35f4-0310-b1b3-9a3be3380cfb

---
+++ lib/fields.cc
@@ -0,0 +1,17 @@
+#include "fields.h"
+
+#include "result.h"
+
+
+Fields::size_type Fields::size() const 
+{
+	return res->num_fields();
+}
+
+const Field& Fields::operator[](Fields::size_type i) const
+{
+    res->field_seek(i); 
+    return res->fetch_field();
+}
+
+
