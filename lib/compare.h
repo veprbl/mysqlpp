c0e5353767bfbffce88ffe63d90715a04b4e8816
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-05-13 22:07:03 +0000

    cb'd lib/compare.h
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@643 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/compare.h
+++ lib/compare.h
@@ -38,29 +38,47 @@ namespace mysqlpp {
 /// Documentation needed!
 
 template <class BinaryPred, class CmpType>
-class MysqlCmp : public std::unary_function<const Row&, bool>
+class MysqlCmp : public std::unary_function<const Row &, bool>
 {
 protected:
-  unsigned int index;
-  BinaryPred   func;
-  CmpType      cmp2;
+	unsigned int index;
+	BinaryPred func;
+	CmpType cmp2;
+	
 public:
-  MysqlCmp(uint i, const BinaryPred &f, const CmpType &c) : index(i),func(f),cmp2(c) {}
-  bool operator () (const Row& cmp1) const {return func(cmp2,cmp1[this->index]);}
+	MysqlCmp(uint i, const BinaryPred& f, const CmpType& c) :
+	index(i),
+	func(f),
+	cmp2(c)
+	{
+	}
+	
+	bool operator ()(const Row& cmp1) const
+	{
+		return func(cmp2, cmp1[this->index]);
+	}
 };
 
 
 /// Documentation needed!
 
 template <class BinaryPred>
-class MysqlCmpCStr : public MysqlCmp<BinaryPred, const char *>
+class MysqlCmpCStr : public MysqlCmp<BinaryPred, const char*>
 {
 public:
-  MysqlCmpCStr(uint i, const BinaryPred &f, const char* c) : MysqlCmp<BinaryPred, const char *> (i,f,c) {}
-  bool operator () (const Row& cmp1) const 
-    {return MysqlCmp<BinaryPred, const char*>::func(
-			MysqlCmp<BinaryPred, const char*>::cmp2, cmp1[this->index]);}
+	MysqlCmpCStr(uint i, const BinaryPred& f, const char *c) :
+	MysqlCmp<BinaryPred, const char*>(i, f, c)
+	{
+	}
+	
+	bool operator ()(const Row& cmp1) const
+	{
+		return MysqlCmp<BinaryPred, const char*>::func(
+				MysqlCmp<BinaryPred, const char*>::cmp2,
+				cmp1[this->index]);
+	}
 };
+
 
 /// \brief For comparing any two objects, as long as they can be
 /// converted to SQLString.
@@ -76,62 +94,88 @@ public:
 /// \param i field index number
 /// \param func one of the functors in compare.h, or any compatible functor
 /// \param cmp2 what to compare to
-template <class BinaryPred, class CmpType>
-MysqlCmp <BinaryPred, CmpType>
-mysql_cmp(uint i, const BinaryPred &func, const CmpType &cmp2)
+template <class BinaryPred, class CmpType> MysqlCmp<BinaryPred, CmpType>
+mysql_cmp(uint i, const BinaryPred& func, const CmpType& cmp2)
 {
-  return MysqlCmp<BinaryPred, CmpType>(i, func, cmp2);
+	return MysqlCmp<BinaryPred, CmpType>(i, func, cmp2);
 }
 
-typedef std::binary_function<const char*, const char*, bool> bin_char_pred;
+
+typedef std::binary_function<const char*, const char*, bool>
+		bin_char_pred;
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_equal_to : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return !std::strcmp(x,y);}
+struct cstr_equal_to : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return !std::strcmp(x, y);
+	}
 };
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_not_equal_to : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return std::strcmp(x,y) != 0;}
+struct cstr_not_equal_to : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return std::strcmp(x, y) != 0;
+	}
 };
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_less : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return std::strcmp(x,y) > 0; }
+struct cstr_less : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return std::strcmp(x, y) > 0;
+	}
 };
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_less_equal : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return std::strcmp(x,y) >= 0; }
+struct cstr_less_equal : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return std::strcmp(x, y) >= 0;
+	}
 };
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_greater : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return std::strcmp(x,y) < 0; }
+struct cstr_greater : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return std::strcmp(x, y) < 0;
+	}
 };
+
 
 /// \brief Documentation needed!
 ///
 /// Document me!
-struct cstr_greater_equal : bin_char_pred {
-  bool operator () (const char *x, const char *y) const
-    {return std::strcmp(x,y) <= 0; }
+struct cstr_greater_equal : bin_char_pred
+{
+	bool operator ()(const char* x, const char* y) const
+	{
+		return std::strcmp(x, y) <= 0;
+	}
 };
+
 
 /// \brief For comparing anything to a <tt>const char*</tt>.
 ///
@@ -140,16 +184,15 @@ struct cstr_greater_equal : bin_char_pred {
 ///
 /// \param i field index number
 /// \param func one of \c cstr_equal_to(), \c cstr_not_equal_to(),
-///		\c cstr_less(), \c cstr_less_equal(), \c cstr_less_equal(), or
-///		\c cstr_less_equal().
+///     \c cstr_less(), \c cstr_less_equal(), \c cstr_less_equal(), or
+///     \c cstr_less_equal().
 /// \param cmp2 what to compare to
-template <class BinaryPred>
-MysqlCmpCStr <BinaryPred>
-mysql_cmp_cstr (uint i, const BinaryPred &func, const char *cmp2) {
-  return MysqlCmpCStr<BinaryPred>(i, func, cmp2);
+template <class BinaryPred> MysqlCmpCStr<BinaryPred>
+mysql_cmp_cstr(uint i, const BinaryPred& func, const char* cmp2)
+{
+	return MysqlCmpCStr<BinaryPred>(i, func, cmp2);
 }
 
 } // end namespace mysqlpp
 
 #endif
-
