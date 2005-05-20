dc44b18fc7d5ca25a22611cb77f369b8404c8a14
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-05-20 00:31:54 +0000

    - Added a bunch of Doxygen comments to manip.cpp - Changed some uses of "'" to '\'' for efficiency - Removed some useless casts
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@662 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/manip.cpp
+++ lib/manip.cpp
@@ -34,23 +34,33 @@ namespace mysqlpp {
 
 bool dont_quote_auto = false;
 
-// quote manipulator
+/// \brief Inserts a SQLString into a stream, quoted and escaped.
+///
+/// If in.is_string is set and in.dont_escape is \e not set, the string
+/// is quoted and escaped.
+///
+/// If both in.is_string and in.dont_escape are set, the string is
+/// quoted but not escaped.
+///
+/// If in.is_string is not set, the data is inserted as-is.  This is
+/// the case when you initialize SQLString with one of the constructors
+/// taking an integral type, for instance.
 
 SQLQueryParms& operator <<(quote_type2 p, SQLString& in)
 {
 	if (in.is_string) {
 		if (in.dont_escape) {
-			SQLString in2 = "'" + in + "'";
+			SQLString in2 = '\'' + in + '\'';
 			 in2.processed = true;
 			 return *p.qparms << in2;
 		}
 		else {
-			char *s = new char[in.size() * 2 + 1];
-			mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
-			SQLString in2 = SQLString("'") + s + "'";
+			char* s = new char[in.size() * 2 + 1];
+			mysql_escape_string(s, in.c_str(), in.size());
+			SQLString in2 = SQLString('\'') + s + '\'';
 			in2.processed = true;
 			*p.qparms << in2;
-			delete[]s;
+			delete[] s;
 			return *p.qparms;
 		}
 	}
@@ -60,42 +70,57 @@ SQLQueryParms& operator <<(quote_type2 p, SQLString& in)
 	}
 }
 
+
+/// \brief Inserts a C++ string into a stream, quoted and escaped
+///
+/// Because std::string lacks the type information we need, the string
+/// is both quoted and escaped, always.
+
 template<>
 ostream& operator<<(quote_type1 o, const string& in)
 {
-	char *s = new char[in.size() * 2 + 1];
-	mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
-	*o.ostr << "'" << s << "'";
+	char* s = new char[in.size() * 2 + 1];
+	mysql_escape_string(s, in.c_str(),
+			static_cast<unsigned long>(in.size()));
+	*o.ostr << '\'' << s << '\'';
 	delete[] s;
 	return *o.ostr;
 }
 
+
+/// \brief Inserts a C string into a stream, quoted and escaped
+///
+/// Because C strings lack the type information we need, the string
+/// is both quoted and escaped, always.
+
 template<>
 ostream& operator<<(quote_type1 o, const char* const& in)
 {
-	unsigned int size;
-	for (size = 0; in[size]; size++) ;
-	char *s = new char[size * 2 + 1];
-	mysql_escape_string(s, const_cast < char *>(in), size);
-	*o.ostr << "'" << s << "'";
+	unsigned int size = strlen(in);
+	char* s = new char[size * 2 + 1];
+	mysql_escape_string(s, in, size);
+	*o.ostr << '\'' << s << '\'';
 	delete[] s;
 	return *o.ostr;
 }
+
+
+/// \brief Utility function used by operator<<(quote_type1, ColData)
 
 template<class Str>
 inline ostream& _manip(quote_type1 o, const ColData_Tmpl<Str>& in)
 {
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		if (in.quote_q())
-			*o.ostr << "'" << s << "'";
+			*o.ostr << '\'' << s << '\'';
 		else
 			*o.ostr << s;
 		delete[] s;
 	}
 	else if (in.quote_q()) {
-		*o.ostr << "'" << in << "'";
+		*o.ostr << '\'' << in << '\'';
 	}
 	else {
 		*o.ostr << in;
@@ -103,17 +128,40 @@ inline ostream& _manip(quote_type1 o, const ColData_Tmpl<Str>& in)
 	return *o.ostr;
 }
 
+
+/// \brief Inserts a ColData into a stream, quoted and escaped
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote or escape the data, if it is not
+/// needed.
+
 template<>
 ostream& operator<<(quote_type1 o, const ColData_Tmpl<string>& in)
 {
 	return _manip(o, in);
 }
 
+
+/// \brief Inserts a ColData with const string into a stream, quoted and
+/// escaped
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote or escape the data, if it is not
+/// needed.
+
 template<>
 ostream& operator<<(quote_type1 o, const ColData_Tmpl<const_string>& in)
 {
 	return _manip(o, in);
 }
+
+
+/// \brief Inserts a ColData into a stream.
+///
+/// Because ColData was designed to contain MySQL type data, this
+/// operator has the information needed to choose to quote and/or escape
+/// the data as it is inserted into the stream, even if you don't use
+/// any of the quoting or escaping manipulators.
 
 ostream& operator<<(ostream& o, const ColData_Tmpl<string>& in)
 {
@@ -123,16 +171,16 @@ ostream& operator<<(ostream& o, const ColData_Tmpl<string>& in)
 	}
 
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		if (in.quote_q())
-			o << "'" << s << "'";
+			o << '\'' << s << '\'';
 		else
 			o << s;
 		delete[] s;
 	}
 	else if (in.quote_q()) {
-		o << "'" << in.get_string() << "'";
+		o << '\'' << in.get_string() << '\'';
 	}
 	else {
 		o << in.get_string();
@@ -140,6 +188,13 @@ ostream& operator<<(ostream& o, const ColData_Tmpl<string>& in)
 	return o;
 }
 
+
+/// \brief Inserts a ColData with const string into a stream.
+///
+/// Because ColData was designed to contain MySQL type data, this
+/// operator has the information needed to choose to quote and/or escape
+/// the data as it is inserted into the stream, even if you don't use
+/// any of the quoting or escaping manipulators.
 
 ostream& operator<<(ostream& o, const ColData_Tmpl<const_string>& in)
 {
@@ -149,23 +204,29 @@ ostream& operator<<(ostream& o, const ColData_Tmpl<const_string>& in)
 	}
 
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, const_cast < char *>(in.c_str()),
-							in.size());
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		if (in.quote_q())
-			o << "'" << s << "'";
+			o << '\'' << s << '\'';
 		else
 			o << s;
 		delete[] s;
 	}
 	else if (in.quote_q()) {
-		o << "'" << in.get_string() << "'";
+		o << '\'' << in.get_string() << '\'';
 	}
 	else {
 		o << in.get_string();
 	}
 	return o;
 }
+
+
+/// \brief Insert a ColData into a SQLQuery
+///
+/// This operator appears to be a workaround for a weakness in one
+/// compiler's implementation of the C++ type system.  See Wishlist for
+/// current plan on what to do about this.
 
 SQLQuery& operator<<(SQLQuery& o, const ColData_Tmpl<string>& in)
 {
@@ -174,23 +235,29 @@ SQLQuery& operator<<(SQLQuery& o, const ColData_Tmpl<string>& in)
 		return o;
 	}
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		if (in.quote_q())
-			static_cast<ostream &>(o) << "'" << s << "'";
+			static_cast<ostream&>(o) << '\'' << s << '\'';
 		else
-			static_cast<ostream &>(o) << s;
+			static_cast<ostream&>(o) << s;
 		delete[] s;
 	}
 	else if (in.quote_q()) {
-		static_cast<ostream &>(o) << "'" << in.get_string() << "'";
+		static_cast<ostream&>(o) << '\'' << in.get_string() << '\'';
 	}
 	else {
-		static_cast<ostream &>(o) << in.get_string();
+		static_cast<ostream&>(o) << in.get_string();
 	}
 	return o;
 }
 
+
+/// \brief Insert a ColData with const string into a SQLQuery
+///
+/// This operator appears to be a workaround for a weakness in one
+/// compiler's implementation of the C++ type system.  See Wishlist for
+/// current plan on what to do about this.
 
 SQLQuery& operator<<(SQLQuery& o, const ColData_Tmpl<const_string>& in)
 {
@@ -199,30 +266,35 @@ SQLQuery& operator<<(SQLQuery& o, const ColData_Tmpl<const_string>& in)
 		return o;
 	}
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, const_cast < char *>(in.c_str()),
-							in.size());
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		if (in.quote_q())
-			static_cast<ostream &>(o) << "'" << s << "'";
+			static_cast<ostream&>(o) << '\'' << s << '\'';
 		else
-			static_cast<ostream &>(o) << s;
+			static_cast<ostream&>(o) << s;
 		delete[] s;
 	}
 	else if (in.quote_q()) {
-		static_cast<ostream &>(o) << "'" << in.get_string() << "'";
+		static_cast<ostream&>(o) << '\'' << in.get_string() << '\'';
 	}
 	else {
-		static_cast<ostream &>(o) << in.get_string();
+		static_cast<ostream&>(o) << in.get_string();
 	}
 	return o;
 }
 
-// quote only manipulator
+
+/// \brief Inserts a SQLString into a stream, quoting it unless it's
+/// data that needs no quoting.
+///
+/// We make the decision to quote the data based on the in.is_string
+/// flag.  You can set it yourself, but SQLString's ctors should set
+/// it correctly for you.
 
 SQLQueryParms& operator<<(quote_only_type2 p, SQLString& in)
 {
 	if (in.is_string) {
-		SQLString in2 = "'" + in + "'";
+		SQLString in2 = '\'' + in + '\'';
 		in2.processed = true;
 		return *p.qparms << in2;
 	}
@@ -232,24 +304,36 @@ SQLQueryParms& operator<<(quote_only_type2 p, SQLString& in)
 	}
 }
 
+
+/// \brief Inserts a ColData into a stream, quoted
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote the data, if it is not needed.
+
 template<>
 ostream& operator<<(quote_only_type1 o, const ColData_Tmpl<string>& in)
 {
 	if (in.quote_q()) {
-		*o.ostr << "'" << in << "'";
+		*o.ostr << '\'' << in << '\'';
 	}
 	else {
 		*o.ostr << in;
 	}
 	return *o.ostr;
 }
+
+
+/// \brief Inserts a ColData with const string into a stream, quoted
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote the data, if it is not needed.
 
 template<>
 ostream& operator<<(quote_only_type1 o,
 		const ColData_Tmpl<const_string>& in)
 {
 	if (in.quote_q()) {
-		*o.ostr << "'" << in << "'";
+		*o.ostr << '\'' << in << '\'';
 	}
 	else {
 		*o.ostr << in;
@@ -257,7 +341,13 @@ ostream& operator<<(quote_only_type1 o,
 	return *o.ostr;
 }
 
-// quote double only manipulator
+
+/// \brief Inserts a SQLString into a stream, double-quoting it (")
+/// unless it's data that needs no quoting.
+///
+/// We make the decision to quote the data based on the in.is_string
+/// flag.  You can set it yourself, but SQLString's ctors should set
+/// it correctly for you.
 
 SQLQueryParms& operator<<(quote_double_only_type2 p, SQLString& in)
 {
@@ -273,25 +363,37 @@ SQLQueryParms& operator<<(quote_double_only_type2 p, SQLString& in)
 }
 
 
+/// \brief Inserts a ColData into a stream, double-quoted (")
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote the data, if it is not needed.
+
 template<>
 ostream& operator<<(quote_double_only_type1 o,
 		const ColData_Tmpl<string>& in)
 {
 	if (in.quote_q()) {
-		*o.ostr << "'" << in << "'";
+		*o.ostr << '\'' << in << '\'';
 	}
 	else {
 		*o.ostr << in;
 	}
 	return *o.ostr;
 }
+
+
+/// \brief Inserts a ColData with const string into a stream,
+/// double-quoted (")
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to actually quote the data, if it is not needed.
 
 template<>
 ostream& operator<<(quote_double_only_type1 o,
 		const ColData_Tmpl<const_string>& in)
 {
 	if (in.quote_q()) {
-		*o.ostr << "'" << in << "'";
+		*o.ostr << '\'' << in << '\'';
 	}
 	else {
 		*o.ostr << in;
@@ -299,24 +401,24 @@ ostream& operator<<(quote_double_only_type1 o,
 	return *o.ostr;
 }
 
-// escape manipulator
+
+/// \brief Inserts a SQLString into a stream, escaping special SQL
+/// characters
+///
+/// We actually only do the escaping if in.is_string is set but
+/// in.dont_escape is not.  If that is not the case, we insert the
+/// string data directly.
 
 SQLQueryParms& operator<<(escape_type2 p, SQLString& in)
 {
-	if (in.is_string) {
-		if (in.dont_escape) {
-			in.processed = true;
-			return *p.qparms << in;
-		}
-		else {
-			char *s = new char[in.size() * 2 + 1];
-			mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
-			SQLString in2 = s;
-			in2.processed = true;
-			*p.qparms << in2;
-			delete[] s;
-			return *p.qparms;
-		}
+	if (in.is_string && ! in.dont_escape) {
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
+		SQLString in2 = s;
+		in2.processed = true;
+		*p.qparms << in2;
+		delete[] s;
+		return *p.qparms;
 	}
 	else {
 		in.processed = true;
@@ -324,35 +426,52 @@ SQLQueryParms& operator<<(escape_type2 p, SQLString& in)
 	}
 }
 
+
+/// \brief Inserts a C++ string into a stream, escaping special SQL
+/// characters
+///
+/// Because std::string lacks the type information we need, the string
+/// is always escaped, even if it doesn't need it.
+
 template<>
 std::ostream& operator<<(escape_type1 o, const std::string& in)
 {
-	char *s = new char[in.size() * 2 + 1];
-	mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
+	char* s = new char[in.size() * 2 + 1];
+	mysql_escape_string(s, in.c_str(),
+			static_cast<unsigned long>(in.size()));
 	*o.ostr << s;
 	delete[] s;
 	return *o.ostr;
 }
+
+
+/// \brief Inserts a C string into a stream, escaping special SQL
+/// characters
+///
+/// Because C's type system lacks the information we need to second-
+/// guess this manipulator, we always run the escaping algorithm on
+/// the data, even if it's not needed.
 
 template<>
 ostream& operator<<(escape_type1 o, const char* const& in)
 {
-	unsigned int size;
-	for (size = 0; in[size]; size++) ;
-	char *s = new char[size * 2 + 1];
-	mysql_escape_string(s, const_cast < char *>(in), size);
+	unsigned int size = strlen(in);
+	char* s = new char[size * 2 + 1];
+	mysql_escape_string(s, in, size);
 	*o.ostr << s;
 	delete[] s;
 	return *o.ostr;
 }
 
+
+/// \brief Utility function used by operator<<(escape_type1, ColData)
 
 template<class Str>
 inline ostream& _manip(escape_type1 o, const ColData_Tmpl<Str>& in)
 {
 	if (in.escape_q()) {
-		char *s = new char[in.size() * 2 + 1];
-		mysql_escape_string(s, in.c_str(), static_cast <unsigned long> (in.size()));
+		char* s = new char[in.size() * 2 + 1];
+		mysql_escape_string(s, in.c_str(), in.size());
 		delete[] s;
 	}
 	else {
@@ -361,11 +480,25 @@ inline ostream& _manip(escape_type1 o, const ColData_Tmpl<Str>& in)
 	return *o.ostr;
 }
 
+
+/// \brief Inserts a ColData into a stream, escaping special SQL
+/// characters
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to escape the data, if it is not needed.
+
 template<>
 std::ostream& operator<<(escape_type1 o, const ColData_Tmpl<std::string>& in)
 {
 	return _manip(o, in);
 }
+
+
+/// \brief Inserts a ColData with const string into a stream, escaping
+/// special SQL characters
+///
+/// Because ColData was designed to contain MySQL type data, we may
+/// choose not to escape the data, if it is not needed.
 
 template<>
 std::ostream& operator<<(escape_type1 o, const ColData_Tmpl<const_string>& in)
