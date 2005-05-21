fff1741a72ffb867f90ea7ac2634f262b090aebc
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-05-21 01:30:54 +0000

    cb'd lib/datetime.h
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@680 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/datetime.h
+++ lib/datetime.h
@@ -42,48 +42,54 @@
 namespace mysqlpp {
 
 /// \brief Base class for mysql_date and mysql_time
-struct mysql_dt_base {
-  virtual std::ostream& out_stream(std::ostream&) const = 0;
-
-  operator std::string () const;
+struct mysql_dt_base
+{
+	/// \if INTERNAL
+	// Make Doxygen ignore this function; struct docs are sufficient
+	virtual std::ostream& out_stream(std::ostream&) const = 0;
+	operator std::string() const;
+	/// \endif
 };
+
 
 /// \if INTERNAL
 // Doxygen will not generate documentation for this section.
 
-template <class T>
-struct DTbase {
-  virtual short int compare(const T &other) const = 0;
+template <class T> struct DTbase
+{
+	virtual short int compare(const T& other) const = 0;
 
-  bool operator == (const T &other) const {return !compare(other);}
-  bool operator != (const T &other) const {return compare(other);}
-  bool operator <  (const T &other) const {return compare(other) < 0;}
-  bool operator <= (const T &other) const {return compare(other) <= 0;}
-  bool operator >  (const T &other) const {return compare(other) > 0;}
-  bool operator >= (const T &other) const {return compare(other) >= 0;}
+	bool operator ==(const T& other) const { return !compare(other); }
+	bool operator !=(const T& other) const { return compare(other); }
+	bool operator <(const T& other) const { return compare(other) < 0; }
+	bool operator <=(const T& other) const { return compare(other) <= 0; }
+	bool operator >(const T& other) const { return compare(other) > 0; }
+	bool operator >=(const T& other) const { return compare(other) >= 0; }
 };
 
 /// \endif
 
 
 /// \brief Base class of Date
-struct mysql_date : virtual public mysql_dt_base {
-  short int year;
-  tiny_int  month;
-  tiny_int  day;
+struct mysql_date : virtual public mysql_dt_base
+{
+	short int year;
+	tiny_int month;
+	tiny_int day;
 
-  /// \brief Insert the date into a stream
-  ///
-  /// The date is inserted into the stream in a format that the MySQL
-  /// server can accept.
-  ///
-  /// \param os stream to insert date into
-  std::ostream& out_stream(std::ostream& os) const;
+	/// \brief Insert the date into a stream
+	///
+	/// The date is inserted into the stream in a format that the MySQL
+	/// server can accept.
+	///
+	/// \param os stream to insert date into
+	std::ostream& out_stream(std::ostream& os) const;
 
-  /// \brief Parse a MySQL date string into this object.
-  cchar* convert (cchar*);
+	/// \brief Parse a MySQL date string into this object.
+	cchar* convert(cchar*);
+
 protected:
-  short int compare(const mysql_date *other) const;
+	short int compare(const mysql_date* other) const;
 };
 
 
@@ -91,21 +97,30 @@ protected:
 ///
 /// Objects of this class can be inserted into streams, and
 /// initialized from a stream.
+
 struct Date : public mysql_date, public DTbase<Date>
 {
-  Date () {};
-  Date (cchar* str) {convert(str);}
-  Date (const ColData &str);
-  Date (const std::string &str);
+	Date() { };
 
-  short int compare(const Date& other) const 
-  {
-  	return mysql_date::compare(&other);
-  }
+	Date(cchar* str)
+	{
+		convert(str);
+	}
+	
+	Date(const ColData& str);
+
+	Date(const std::string& str);
+
+	short int compare(const Date& other) const
+	{
+		return mysql_date::compare(&other);
+	}
 };
+
 
 /// \brief Inserts a Date object into a C++ stream in a MySQL-compatible
 /// format.
+
 inline std::ostream& operator <<(std::ostream& s, const Date& d)
 {
 	return d.out_stream(s);
@@ -113,24 +128,27 @@ inline std::ostream& operator <<(std::ostream& s, const Date& d)
 
 
 /// \brief Base class of Time
-struct mysql_time : virtual public mysql_dt_base {
-  tiny_int hour;
-  tiny_int minute;  
-  tiny_int second;
 
-  /// \brief Insert the time into a stream
-  ///
-  /// The time is inserted into the stream in a format that the MySQL
-  /// server can accept.
-  ///
-  /// \param os stream to insert time into
-  std::ostream& out_stream(std::ostream& os) const;
+struct mysql_time : virtual public mysql_dt_base
+{
+	tiny_int hour;
+	tiny_int minute;
+	tiny_int second;
 
-  /// \brief Parse a MySQL time string into this object.
-  cchar* convert (cchar*);
+	/// \brief Insert the time into a stream
+	///
+	/// The time is inserted into the stream in a format that the MySQL
+	/// server can accept.
+	///
+	/// \param os stream to insert time into
+	std::ostream& out_stream(std::ostream& os) const;
+
+	/// \brief Parse a MySQL time string into this object.
+	cchar* convert(cchar*);
+
 protected:
-  short int compare(const mysql_time *other) const;
-};  
+	short int compare(const mysql_time* other) const;
+};
 
 
 /// \brief Holds MySQL times.
@@ -139,17 +157,27 @@ protected:
 /// initialized from a stream.
 struct Time : public mysql_time, public DTbase<Time>
 {
-  Time () {};
-  Time (cchar* str) {convert(str);}
-  Time (const ColData &str);
-  Time (const std::string &str);
+	Time() { };
 
-  short int compare(const Time& other) const 
-    {return mysql_time::compare(&other);}
+	Time(cchar* str)
+	{
+		convert(str);
+	}
+
+	Time(const ColData& str);
+
+	Time(const std::string& str);
+
+	short int compare(const Time& other) const
+	{
+		return mysql_time::compare(&other);
+	}
 };
+
 
 /// \brief Inserts a Time object into a C++ stream in a MySQL-compatible
 /// format.
+
 inline std::ostream& operator <<(std::ostream& s, const Time& d)
 {
 	return d.out_stream(s);
@@ -161,27 +189,35 @@ inline std::ostream& operator <<(std::ostream& s, const Time& d)
 ///
 /// Objects of this class can be inserted into streams, and
 /// initialized from a stream.
+
 struct DateTime : public mysql_date, public mysql_time,
-		       public DTbase<DateTime>
+		public DTbase<DateTime>
 {
-  DateTime () {}
-  DateTime (cchar* str) {convert(str);}
-  DateTime (const ColData &str);
-  DateTime (const std::string &str);
+	DateTime() { }
+	
+	DateTime(cchar* str)
+	{
+		convert(str);
+	}
+	
+	DateTime(const ColData& str);
 
-  short int compare(const DateTime& other) const;
+	DateTime(const std::string& str);
 
-  /// \brief Insert the date and time into a stream
-  ///
-  /// The date and time are inserted into the stream, in that order,
-  /// with a space between them.
-  ///
-  /// \param os stream to insert date and time into
-  std::ostream& out_stream(std::ostream& os) const;
+	short int compare(const DateTime& other) const;
 
-  /// \brief Parse a MySQL date and time string into this object.
-  cchar* convert (cchar*);
+	/// \brief Insert the date and time into a stream
+	///
+	/// The date and time are inserted into the stream, in that order,
+	/// with a space between them.
+	///
+	/// \param os stream to insert date and time into
+	std::ostream& out_stream(std::ostream& os) const;
+
+	/// \brief Parse a MySQL date and time string into this object.
+	cchar* convert(cchar*);
 };
+
 
 /// \brief Inserts a DateTime object into a C++ stream in a
 /// MySQL-compatible format.
@@ -190,37 +226,44 @@ inline std::ostream& operator <<(std::ostream& s, const DateTime& d)
 	return d.out_stream(s);
 }
 
+
 inline mysql_dt_base::operator std::string() const
 {
-  return stream2string<std::string>(*this);
+	return stream2string<std::string>(*this);
 }
 
-inline Date::Date(const ColData &str)
+
+inline Date::Date(const ColData& str)
 {
 	convert(str.c_str());
 }
 
-inline Date::Date(const std::string &str)
+
+inline Date::Date(const std::string& str)
 {
 	convert(str.c_str());
 }
 
-inline Time::Time(const ColData &str)
+
+inline Time::Time(const ColData& str)
 {
 	convert(str.c_str());
 }
 
-inline Time::Time(const std::string &str)
+
+inline Time::Time(const std::string& str)
 {
 	convert(str.c_str());
 }
 
-inline DateTime::DateTime(const ColData &str)
+
+inline DateTime::DateTime(const ColData& str)
 {
 	convert(str.c_str());
 }
 
-inline DateTime::DateTime(const std::string &str)
+
+inline DateTime::DateTime(const std::string& str)
 {
 	convert(str.c_str());
 }
@@ -228,4 +271,3 @@ inline DateTime::DateTime(const std::string &str)
 } // end namespace mysqlpp
 
 #endif // !defined(MYSQLPP_DATETIME_H)
-
