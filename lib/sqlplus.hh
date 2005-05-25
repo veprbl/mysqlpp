a3e8fde429308d648dd6fa64afd03730cae5850b
tangent <tangent@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-05-25 01:52:43 +0000

    Yet another sqlplus.hh fix.  Doxygen stuff well and truly gone now.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@696 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/sqlplus.hh
+++ lib/sqlplus.hh
@@ -1,7 +1,6 @@
-/// \file sqlplus.hh
-/// \brief Deprecated backwards-compatibility header. Use mysql++.h in
-/// new code instead.
-
+// Do not add a Doxygen \file comment.  It just takes extra time to
+// parse this file (and it's one of the biggest ones, because mysql++.h
+// includes almost all the other files!), with no serious benefit.
 #warning This header is obsolete. Please use mysql++.h instead.
 #include "mysql++.h"
 using namespace mysqlpp;
