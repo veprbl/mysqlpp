6fe1877c27cff3265f716775ac4c895221162a1c
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-06-28 07:54:21 +0000

    Extracted SQLParseElement and SQLQueryParms from sql_query module, and created qparms module for them.  This is a precursor to folding SQLQuery and Query into a single class; there's not much reason to put these classes into the query module, too.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@833 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/sql_query.h
+++ lib/sql_query.h
@@ -1,18 +1,6 @@
 /// \file sql_query.h
-/// \brief Declares the base class for mysqlpp::Query, plus some
-/// utility classes to be used with it.
+/// \brief Declares the base class for mysqlpp::Query.
 ///
-/// Class SQLQuery contains a large part of the functionality of class
-/// Query, which is the only thing that derives from this class.  It is
-/// separate for historical reasons only: early on, there was a dream
-/// (and some effort) to make MySQL++ database-independent.  Once
-/// maintainership shifted to MySQL AB employees in 1999, though, that
-/// dream died.
-///
-/// The current maintainers have no wish to try and revive that dream,
-/// so at some point this class's contents will be folded into the Query
-/// class. This will probably happen in the next major release, when
-/// major ABI breakage is acceptable.
 
 /***********************************************************************
  Copyright (c) 1998 by Kevin Atkinson, (c) 1999, 2000 and 2001 by
@@ -42,6 +30,7 @@
 #define MYSQLPP_SQL_QUERY_H
 
 #include "defs.h"
+#include "qparms.h"
 #include "sql_string.h"
 #include "tiny_int.h"
 
@@ -79,235 +68,8 @@
 
 namespace mysqlpp {
 
-class Query;
-class SQLQuery;
-
-
-/// \brief This class holds the parameter values for filling
-/// template queries. 
-class SQLQueryParms : public std::vector<SQLString>
-{
-private:
-	friend class Query;
-
-	typedef const SQLString& ss;
-	SQLQuery *parent;
-
-public:
-	/// \brief Default constructor
-	SQLQueryParms() :
-	parent(0)
-	{
-	}
-	
-	/// \brief Create object
-	///
-	/// \param p pointer to the query object these parameters are tied
-	/// to
-	SQLQueryParms(SQLQuery* p) :
-	parent(p)
-	{
-	}
-	
-	/// \brief Returns true if we are bound to a query object.
-	///
-	/// Basically, this tells you which of the two ctors were called.
-	bool bound()
-	{
-		return parent != 0;
-	}
-
-	/// \brief Clears the list
-	void clear()
-	{
-		erase(begin(), end());
-	}
-
-	/// \brief Access element number n
-	SQLString& operator [](size_type n)
-	{
-		if (n >= size())
-			insert(end(), (n + 1) - size(), "");
-		return std::vector<SQLString>::operator [](n);
-	}
-
-	/// \brief Access element number n
-	const SQLString& operator [](size_type n) const
-	{
-		return std::vector<SQLString>::operator [](n);
-	}
-	
-	/// \brief Access the value of the element with a key of str.
-	SQLString& operator [](const char *str);
-
-	/// \brief Access the value of the element with a key of str.
-	const SQLString& operator [](const char *str) const;
-
-	/// \brief Adds an element to the list
-	SQLQueryParms& operator <<(const SQLString& str)
-	{
-		push_back(str);
-		return *this;
-	}
-
-	/// \brief Adds an element to the list
-	SQLQueryParms& operator +=(const SQLString& str)
-	{
-		push_back(str);
-		return *this;
-	}
-
-	/// \brief Build a composite of two parameter lists
-	///
-	/// If this list is (a, b) and \c other is (c, d, e, f, g), then
-	/// the returned list will be (a, b, e, f, g).  That is, all of this
-	/// list's parameters are in the returned list, plus any from the
-	/// other list that are in positions beyond what exist in this list.
-	///
-	/// If the two lists are the same length or this list is longer than
-	/// the \c other list, a copy of this list is returned.
-	SQLQueryParms operator +(const SQLQueryParms& other) const;
-
-#if !defined(DOXYGEN_IGNORE)
-// Doxygen will not generate documentation for this section.
-	void set(ss a)
-	{
-		clear();
-		*this << a;
-	}
-	void set(ss a, ss b)
-	{
-		clear();
-		*this << a << b;
-	}
-	void set(ss a, ss b, ss c)
-	{
-		clear();
-		*this << a << b << c;
-	}
-	void set(ss a, ss b, ss c, ss d)
-	{
-		clear();
-		*this << a << b << c << d;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e)
-	{
-		clear();
-		*this << a << b << c << d << e;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f)
-	{
-		clear();
-		*this << a << b << c << d << e << f;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g, ss h)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g << h;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g, ss h, ss i)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g << h << i;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g, ss h, ss i,
-			 ss j)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g << h << i << j;
-	}
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g, ss h, ss i,
-			 ss j, ss k)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g << h << i << j << k;
-	}
-#endif // !defined(DOXYGEN_IGNORE)
-
-	/// \brief Set the template query parameters.
-	///
-	/// Sets parameter 0 to a, parameter 1 to b, etc. There are
-	/// overloaded versions of this function that take anywhere from
-	/// one to a dozen parameters.
-	void set(ss a, ss b, ss c, ss d, ss e, ss f, ss g, ss h, ss i,
-			 ss j, ss k, ss l)
-	{
-		clear();
-		*this << a << b << c << d << e << f << g << h << i << j << k << l;
-	}
-};
-
-
 /// \brief Used for indicating whether a query object should auto-reset
-/// or not.
 enum query_reset { DONT_RESET, RESET_QUERY };
-
-
-/// \brief Used within SQLQuery to hold elements for parameterized
-/// queries.
-///
-/// Each element has three parts:
-///
-/// The concept behind the \c before variable needs a little explaining.
-/// When a template query is parsed, each parameter is parsed into one
-/// of these SQLParseElement objects, but the non-parameter parts of the
-/// template also have to be stored somewhere.  MySQL++ chooses to
-/// attach the text leading up to a parameter to that parameter.  So,
-/// the \c before string is simply the text copied literally into the
-/// finished query before we insert a value for the parameter.
-///
-/// The \c option character is currently one of 'q', 'Q', 'r', 'R' or
-/// ' '.  See the "Template Queries" chapter in the user manual for
-/// details.
-///
-/// The position value (\c num) allows a template query to have its
-/// parameters in a different order than in the Query method call.
-/// An example of how this can be helpful is in the "Template Queries"
-/// chapter of the user manual.
-
-struct SQLParseElement {
-	/// \brief Create object
-	///
-	/// \param b the 'before' value
-	/// \param o the 'option' value
-	/// \param n the 'num' value
-	SQLParseElement(std::string b, char o, char n) :
-	before(b),
-	option(o),
-	num(n)
-	{
-	}
-	
-	std::string before;		///< string inserted before the parameter
-	char option;			///< the parameter option, or blank if none
-	char num;				///< the parameter position to use
-};
-
-
-/** \brief The base class for mysqlpp::Query.
-
-	One uses an object of this class to form queries that can be
-	sent to the database server via the mysqlpp::Connection object.
-
-	This class is subclassed from \c std::stringstream. This means
-	that you can form a SQL query using C++ stream idioms without
-	having to create your own \c stringstream object and then dump
-	that into the query object.  And of course, it gets you all the
-	benefits of C++ streams, such as type safety, which \c sprintf()
-	and such do not offer.	Although you can read from this object
-	as you would any other stream, this is \e not recommended. It
-	may fail in strange ways, and there is no support offered if
-	you break it by doing so.
-
-	If you seek within the stream in any way, be sure to reset
-	the stream pointer to the end before calling any of the
-	SQLQuery-specific methods except for error() and success().
-*/
 
 class SQLQuery : public std::stringstream
 {
@@ -500,23 +262,6 @@ public:
 	mysql_query_define0(std::string, str);
 #endif // !defined(DOXYGEN_IGNORE)
 };
-
-
-inline SQLString& SQLQueryParms::operator [](const char* str)
-{
-	if (parent) {
-		return operator [](parent->parsed_nums[str]);
-	}
-	throw; // only here temporary 
-}
-
-inline const SQLString& SQLQueryParms::operator[] (const char* str) const
-{
-	if (parent) {
-		return operator [](parent->parsed_nums[str]);
-	}
-	throw; // only here temporary 
-}
 
 } // end namespace mysqlpp
 
