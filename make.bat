d79ae7d02a44154fa65ca40b830b82eb84541676
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-07-19 22:12:17 +0000

    .
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@954 5946f024-35f4-0310-b1b3-9a3be3380cfb

---
+++ make.bat
@@ -0,0 +1,43 @@
+@echo off
+
+rem Command line parsing
+:reparse
+if "%1" == "clean" goto do_clean
+if not exist lib\Makefile goto error
+
+
+rem Build the library file
+cmd /c cd lib ^& make.bat %*
+if errorlevel 1 goto end
+echo.
+echo MySQL++ library built successfully.
+echo.
+copy lib\*.dll examples > NUL
+
+cmd /c cd examples ^& make.bat %*
+if errorlevel 1 goto end
+echo.
+echo MySQL++ examples built successfully.
+echo.
+goto end
+
+
+rem Remove generated files
+:do_clean
+if exist lib\Makefile cmd /c cd lib ^& make clean
+del lib\Makefile lib\make.bat
+if exist examples\Makefile cmd /c cd examples ^& make clean
+del examples\Makefile examples\make.bat
+shift
+if "%1" NEQ "" goto reparse
+goto end
+
+
+rem Display error message
+:error
+echo.
+echo You must run makemake first!
+goto end
+
+
+:end
