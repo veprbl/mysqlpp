704a6f7270342d7b19c565a8b2e1088643d6b28a
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-07-19 22:08:25 +0000

    Split make.bat into makemake.bat and make.bat.  The latter calls the former, so the usage is the same, but each is now less cluttered than when one script did both jobs.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@952 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- makemake.bat
+++ makemake.bat
@@ -1,50 +1,8 @@
 @echo off
 
-rem Command line parsing
-:reparse
-if "%1" == "clean" goto do_clean
+rem Figure out which compiler the user wants to create Makefiles for
 if "%1" == "vc" goto build_makefiles
-if not exist lib\Makefile.%1 goto usage
-
-
-rem Build the library file
-cmd /c cd lib ^& make.bat %*
-if errorlevel 1 goto end
-echo.
-echo MySQL++ library built successfully.
-echo.
-copy lib\*.dll examples > NUL
-
-cmd /c cd examples ^& make.bat %*
-if errorlevel 1 goto end
-echo.
-echo MySQL++ examples built successfully.
-echo.
-goto end
-
-
-rem Display usage message
-:usage
-echo usage: make {vc} [args]
-echo.
-echo     The first time you run this, you must give one of the compiler
-echo     parameters as the first argument, so the script can build the
-echo     proper Makefiles for your system.
-echo.
-echo     vc: build with Visual C++ command-line compiler and nmake
-echo.
-goto end
-
-
-rem Remove generated files
-:do_clean
-if exist lib\Makefile cmd /c cd lib ^& make clean
-del lib\Makefile lib\make.bat
-if exist examples\Makefile cmd /c cd examples ^& make clean
-del examples\Makefile examples\make.bat
-shift
-if "%1" NEQ "" goto reparse
-goto end
+goto usage
 
 
 rem Set up common headers for new Makefiles
@@ -54,7 +12,7 @@ set head2=## Edit those files instead, then type 'make' to rebuild
 set head3=## this Makefile.
 
 rem Decide how to build subordinate make.bat files
-if "%1"=="vc" goto vc_make
+if "%1" == "vc" goto vc_make
 goto generic_make
 :vc_make
 echo @nmake /nologo ^%* > lib\make.bat
@@ -83,7 +41,18 @@ echo. >> examples\Makefile
 type examples\Makefile.%1 >> examples\Makefile
 type examples\Makefile.base >> examples\Makefile
 shift
-goto reparse
+call make.bat
+
+
+rem Display usage message
+:usage
+echo usage: makemake {vc} [args]
+echo.
+echo     You must give one of the compiler parameters:
+echo.
+echo         vc: build with Visual C++ command-line compiler and nmake
+echo.
+goto end
 
 
 :end
