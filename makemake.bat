421c331483fc7879a94bad47f779c995a2e75e4b
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-09-03 06:22:41 +0000

    First cut at making VC++ build use GNU make instead of nmake.  It works, but it doesn't yet have any new features beyond old system.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1083 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- makemake.bat
+++ makemake.bat
@@ -7,51 +7,24 @@ if "%1" == "mingw" goto build_makefiles
 goto usage
 
 
-rem Set up common headers for new Makefiles
 :build_makefiles
-set head1=## DO NOT EDIT.  Created from Makefile.%1 and Makefile.base.
-set head2=## Edit those files instead, then type 'makemake %1' to rebuild
-set head3=## this Makefile.
+rem Create Makefiles in subdirectories
+for %d in ( lib examples ) do call buildmf.bat %1 %d
 
-rem Decide how to build subordinate make.bat files
-if "%1" == "vc" goto vc_make
-goto generic_make
-:vc_make
-echo @nmake /nologo %%* > lib\make.bat
-echo @nmake /nologo %%* > examples\make.bat
-goto make_bat_created
-:generic_make
-echo @make.exe %%* > lib\make.bat
-echo @make.exe %%* > examples\make.bat
-
-:make_bat_created
-rem Create library Makefile
-echo %head1% > lib\Makefile
-echo %head2% >> lib\Makefile
-echo %head3% >> lib\Makefile
-echo. >> lib\Makefile
-echo. >> lib\Makefile
-type lib\Makefile.%1 >> lib\Makefile
-type lib\Makefile.base >> lib\Makefile
-if "%1" == "vc" type lib\Makefile.rel >> lib\Makefile
-
-rem Create example programs Makefile
-echo %head1% > examples\Makefile
-echo %head2% >> examples\Makefile
-echo %head3% >> examples\Makefile
-echo. >> examples\Makefile
-echo. >> examples\Makefile
-type examples\Makefile.%1 >> examples\Makefile
-type examples\Makefile.base >> examples\Makefile
+rem Create top-level Makefile
+echo all clean install: > Makefile
+echo 	cd lib ^&^& $(MAKE) $@ >> Makefile
+echo 	cd examples ^&^& $(MAKE) $@ >> Makefile
 
 
-rem Create library release subdirs, if it doesn't exist already
+rem Create release and debug subdirs, if they don't exist already
+if not exist lib\debug mkdir lib\debug
 if not exist lib\release mkdir lib\release
 
 
 rem Start build process
 shift
-call make.bat
+make %*
 goto end
 
 
@@ -61,9 +34,12 @@ echo usage: makemake {vc, bc, mingw} [args]
 echo.
 echo     You must give one of the compiler parameters:
 echo.
-echo         vc: Visual C++ command-line compiler (cl) and nmake
-echo         bc: Borland C++ command-line compiler (bcc32) and Borland make
-echo         mingw: MinGW GCC (g++) and GNU make
+echo         vc: Visual C++ command-line compiler (cl)
+echo         bc: Borland C++ command-line compiler (bcc32)
+echo         mingw: MinGW GCC (g++)
+echo.
+echo     Any additional arguments you give will be passed on to the
+echo     make program.
 echo.
 goto end
 
