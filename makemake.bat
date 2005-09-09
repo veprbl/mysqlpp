53410ca68887ca43ecf9b036ab9555fc798155ff
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-09-09 21:24:09 +0000

    Generalized makemake-generated Makefiles so they work with either Cygwin make plus Cygwin tools and shell or MinGW make plus Win32 command shell.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1092 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- makemake.bat
+++ makemake.bat
@@ -1,6 +1,6 @@
 @echo off
 
-BUILDSYS=vc
+set BUILDSYS=vc
 
 :build_makefiles
 rem Create Makefiles in subdirectories
@@ -8,7 +8,8 @@ for %d in ( lib examples ) do call buildmf.bat %BUILDSYS% %d
 
 rem Create top-level Makefile
 echo all: > Makefile
-echo 	cd lib ^&^& $(MAKE) BIN_DIR=debug >> Makefile
+echo 	@cmd /c mkbindirs.bat >> Makefile
+echo 	cd lib ^&^& $(MAKE) BIN_DIR=debug all example_setup >> Makefile
 echo 	cd lib ^&^& $(MAKE) BIN_DIR=release >> Makefile
 echo 	cd examples ^&^& $(MAKE) >> Makefile
 echo. >> Makefile
@@ -18,13 +19,6 @@ echo. >> Makefile
 echo clean debug release: >> Makefile
 echo 	cd lib ^&^& $(MAKE) $@ >> Makefile
 echo 	cd examples ^&^& $(MAKE) $@ >> Makefile
-
-
-rem Create release and debug subdirs, if they don't exist already
-if not exist lib\debug mkdir lib\debug
-if not exist lib\release mkdir lib\release
-if not exist examples\debug mkdir examples\debug
-if not exist examples\release mkdir examples\release
 
 
 echo Created Makefiles successfully.
