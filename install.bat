54df39242d2f09c81123ebfc7e2409ac8822ca1f
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2005-09-13 19:29:52 +0000

    Added mingw support to install.bat
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1110 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- install.bat
+++ install.bat
@@ -10,12 +10,20 @@ if not exist %INST_INC_DIR% mkdir %INST_INC_DIR%
 if not exist %INST_LIB_DIR% mkdir %INST_LIB_DIR%
 if not exist %INST_LIB_DIR%\debug mkdir %INST_LIB_DIR%\debug
 if not exist %INST_LIB_DIR%\release mkdir %INST_LIB_DIR%\release
+
 copy lib\*.h "%INST_INC_DIR%" > NUL
+
+if exist lib\debug\*.a goto install_mingw
 copy lib\debug\*.dll "%INST_LIB_DIR%\debug" > NUL
 copy lib\debug\*.lib "%INST_LIB_DIR%\debug" > NUL
 copy lib\release\*.dll "%INST_LIB_DIR%\release" > NUL
 copy lib\release\*.lib "%INST_LIB_DIR%\release" > NUL
+goto install_done
+:install_mingw
+copy lib\debug\*.a "%INST_LIB_DIR%\debug" > NUL
+copy lib\release\*.a "%INST_LIB_DIR%\release" > NUL
 
+:install_done
 echo MySQL++ (%1 version) installed successfully!
 goto end
 
