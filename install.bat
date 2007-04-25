65e46f88f460c81fe984fa19b40eeb09215ebb7c
wyoung <wyoung@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2007-04-25 05:40:23 +0000

    Changed paths in install.bat to catch up with the directory scheme adopted by unifying the library and example Bakefiles.
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1547 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- install.bat
+++ install.bat
@@ -13,15 +13,15 @@ if not exist %INST_LIB_DIR%\release mkdir %INST_LIB_DIR%\release
 
 copy lib\*.h "%INST_INC_DIR%" > NUL
 
-if exist lib\debug\*.a goto install_mingw
-copy lib\debug\*.dll "%INST_LIB_DIR%\debug" > NUL
-copy lib\debug\*.lib "%INST_LIB_DIR%\debug" > NUL
-copy lib\release\*.dll "%INST_LIB_DIR%\release" > NUL
-copy lib\release\*.lib "%INST_LIB_DIR%\release" > NUL
+if exist debug\*.a goto install_mingw
+copy debug\*.dll "%INST_LIB_DIR%\debug" > NUL
+copy debug\*.lib "%INST_LIB_DIR%\debug" > NUL
+copy release\*.dll "%INST_LIB_DIR%\release" > NUL
+copy release\*.lib "%INST_LIB_DIR%\release" > NUL
 goto install_done
 :install_mingw
-copy lib\debug\*.a "%INST_LIB_DIR%\debug" > NUL
-copy lib\release\*.a "%INST_LIB_DIR%\release" > NUL
+copy debug\*.a "%INST_LIB_DIR%\debug" > NUL
+copy release\*.a "%INST_LIB_DIR%\release" > NUL
 
 :install_done
 echo MySQL++ (%1 version) installed successfully!
