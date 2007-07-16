cc860bb663e2960c5be8e491e81f46edf473d53f
wyoung <wyoung@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2007-07-16 18:47:17 +0000

    Comment fix
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1702 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/qparms.cpp
+++ lib/qparms.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************
- qparms.cpp - Implements the SQLQuery class.
+ qparms.cpp - Implements the SQLQueryParms class.
 
  Copyright (c) 1998 by Kevin Atkinson, (c) 1999, 2000 and 2001 by
- MySQL AB, and (c) 2004, 2005 by Educational Technology Resources, Inc.
+ MySQL AB, and (c) 2004-2007 by Educational Technology Resources, Inc.
  Others may also hold copyrights on code in this file.  See the CREDITS
  file in the top directory of the distribution for details.
 
