veprbl/mysqlpp

commit dfd5d3d2877e127a3b4138c16336f32a2ff77d5b

Broke coupling between the lifetime of a ResUse/Result object and the Row objects it creates.  It's now possible to reuse the result object while Row objects it created are still in use.  The only substantial reason for this were things having to do with looking up fields by name: we didn't want to copy the field name list into each Row object, so we had to keep the result object around to reference its copy.
Did we break down and make the wasteful copies?  No, we created
RefCountedPointer<T>, which lets us add reference counting to any object
(like FieldNames) and pass around copies of that object without
duplicating the underlying object unnecessarily.

This template will be used more in the near future.  This is the
beginning of the end for all the return-by-value of large objects
in MySQL++.


git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1751 5946f024-35f4-0310-b1b3-9a3be3380cfb

lib/row.h
@@ -30,6 +30,7 @@
 
 #include "coldata.h"
 #include "noexceptions.h"
+#include "refcounted.h"
 #include "subiter.h"
 #include "vallist.h"
 
@@ -43,7 +44,6 @@ namespace mysqlpp {
 #if !defined(DOXYGEN_IGNORE)
 // Make Doxygen ignore this
 class FieldNames;
-class MYSQLPP_EXPORT ResUse;
 #endif
 
 /// \brief Manages rows from a result set.
@@ -93,19 +93,20 @@ public:
 
 	/// \brief Default constructor
 	Row() :
-	res_(0),
-	initialized_(false)
+	initialized_(false),
+	size_(0)
 	{
 	}
 	
 	/// \brief Copy constructor
 	Row(const Row& r) :
 	data_(r.data_.begin(), r.data_.end()),
-	res_(r.res_),
-	initialized_(r.initialized_)
+	field_names_(r.field_names_),
+	initialized_(r.initialized_),
+	size_(r.size_)
 	{
 	}
-	
+
 	/// \brief Create a row object
 	///
 	/// \param d MySQL C API row data
@@ -118,21 +119,16 @@ public:
 	/// \brief Destroy object
 	~Row() { }
 
-	/// \brief Get a reference to our parent class.
-	const ResUse& parent() const
-	{
-		return *res_;
-	}
-
 	/// \brief Get the number of fields in the row.
-	size_type size() const;
+	size_type size() const { return size_; }
 
 	/// \brief Assignment operator
 	Row& operator=(const Row& rhs)
 	{
 		data_.assign(rhs.data_.begin(), rhs.data_.end());
-		res_ = rhs.res_;
+		field_names_.assign(rhs.field_names_);
 		initialized_ = rhs.initialized_;
+		size_ = rhs.size_;
 		return *this;
 	}
 
@@ -141,44 +137,25 @@ public:
 	/// If the field does not exist in this row, we throw a BadFieldName
 	/// exception.
 	///
-	/// For this operator to work, the Result or ResUse object that
-	/// created this object must still exist.  In other words, you
-	/// cannot re-use or destroy the result object until you are done
-	/// retrieving data from this row object if you look fields up
-	/// in the result by name.  Note that it is the lookup-by-name
-	/// aspect of things that matters here, not the row indexing
-	/// operation itself; this means you can avoid this restriction by
-	/// using \c operator[](int) instead.
-	///
-	/// Another reason to use operator[](int) or at(int) instead is
-	/// efficiency: looking up a field by name is slower than looking
-	/// it up by position within the row.
+	/// This operator is fairly inefficient.  operator[](int) is faster.
 	const value_type& operator [](const char* field) const;
 
 	/// \brief Get the value of a field given its index.
 	///
 	/// This function is just syntactic sugar, wrapping the at() method.
-	///
-	/// \sa at() for the full documentation for this operator, and
-	/// operator[](const char*) for further caveats about using this
-	/// operator.
 	const value_type& operator [](int i) const { return at(i); }
 
 	/// \brief Get the value of a field given its index.
 	///
 	/// If the index value is bad, the underlying std::vector is
 	/// supposed to throw an exception, according to the Standard.
-	///
-	/// For this function to work, the Result or ResUse object that
-	/// created this object must still exist.  In other words, you
-	/// cannot re-use or destroy the result object until you are done
-	/// retrieving data from this row object.
-	///
-	/// See operator[](const char*) for more caveats.
 	const value_type& at(int i) const { return data_.at(i); }
 
 	/// \brief Returns true if there is data in the row.
 	operator bool() const { return data_.size(); }
+
+	/// \brief Returns a field's index given its name
+	size_type field_num(const char* name) const;
 
 	/// \brief Get a list of the values in this row
 	///
@@ -467,8 +444,9 @@ public:
 
 private:
 	std::vector<value_type> data_;
-	const ResUse* res_;
+	RefCountedPointer<FieldNames> field_names_;
 	bool initialized_;
+	size_type size_;
 };
 
 } // end namespace mysqlpp

Wishlist
@@ -214,17 +214,6 @@ v3.0 "Maybe" Stuff
       and another for storing a larger set of bits in a more flexible
       way, perhaps as a BLOB.
 
-    o Row object currently depends on the associated ResUse object
-      to stick around through all calls to .at(), because it needs
-      the list of field types in the result set to construct ColData
-      objects.  This means it's not possible to store the result of
-      several queries before accessing the data.  Currently, this
-      is just a documented limitation, but it would be nice if there
-      were a clean way to avoid this.  Obviously you could just copy
-      the type list when constructing the Row object, but that seems
-      pointlessly inefficient.  Some sort of ownership hand-off if
-      the ResUse object goes away or gets reused first?
-
     o Consider using MySQL C API enum constants in
       mysql_type_info::types definition instead of hard-coded values.
       This could potentially break a lot of infrastructure, though,

examples/fieldinf.cpp
@@ -56,7 +56,7 @@ main(int argc, char *argv[])
 		}
 		cout << endl;
 		
-		for (size_t i = 0; i < res.names().size(); i++) {
+		for (size_t i = 0; i < res.names()->size(); i++) {
 			// Suppress C++ type name outputs when run under dtest,
 			// as they're system-specific.
 			const char* cname = dtest_mode ? "n/a" : res.types(i).name();

examples/multiquery.cpp
@@ -48,7 +48,7 @@ static void
 print_header(IntVectorType& widths, Result& res)
 {
 	cout << "  |" << setfill(' ');
-	for (size_t i = 0; i < res.names().size(); i++) {
+	for (size_t i = 0; i < res.names()->size(); i++) {
 		cout << " " << setw(widths.at(i)) << res.names(i) << " |";
 	}
 	cout << endl;

lib/field_names.h
@@ -47,6 +47,12 @@ class FieldNames : public std::vector<std::string>
 public:
 	/// \brief Default constructor
 	FieldNames() { }
+
+	/// \brief Copy constructor
+	FieldNames(const FieldNames& other)
+	{
+		assign(other.begin(), other.end());
+	}
 	
 	/// \brief Create field name list from a result set
 	FieldNames(const ResUse* res)
@@ -89,9 +95,9 @@ public:
 	}
 
 	/// \brief Get the index number of a field given its name
-	uint operator [](std::string i) const
+	uint operator [](const std::string& s) const
 	{
-		std::string temp(i);
+		std::string temp(s);
 		str_to_lwr(temp);
 		return uint(std::find(begin(), end(), temp) - begin());
 	}

lib/refcounted.h
@@ -1,0 +1,199 @@
+/// \file refcounted.h
+/// \brief Declares the RefCountedPointer template
+
+/***********************************************************************
+ Copyright (c) 2007 by Educational Technology Resources, Inc.
+ Others may also hold copyrights on code in this file.  See the
+ CREDITS file in the top directory of the distribution for details.
+
+ This file is part of MySQL++.
+
+ MySQL++ is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ (at your option) any later version.
+
+ MySQL++ is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with MySQL++; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ USA
+***********************************************************************/
+
+#if !defined(MYSQLPP_REFCOUNTED_H)
+#define MYSQLPP_REFCOUNTED_H
+
+/// \brief Creates an object that acts as a reference-counted pointer
+/// to another object.
+///
+/// Resulting type acts like a pointer in all respects, except that it
+/// manages the memory it points to by observing how many users there
+/// are for the object.
+///
+/// This attempts to be as automatic as reference counting in a
+/// programming language with memory management.  Like all automatic
+/// memory management schemes, it has penalties: it turns the single
+/// indirection of an unmanaged pointer into a double indirection, and
+/// has additional management overhead in the assignment operators due
+/// to the reference counter.  This is an acceptable tradeoff when
+/// wrapping objects that are expensive to copy, and which need to
+/// be "owned" by disparate parties: you can allocate the object just
+/// once, then pass around the reference counted pointer, knowing that
+/// the last user will "turn out the lights".
+
+template <class T>
+class RefCountedPointer
+{
+public:
+	typedef RefCountedPointer<T> ThisType;	///< alias for this object's type
+	
+	/// \brief Standard constructor
+	///
+	/// \param c A pointer to the managed object.  If you leave it at
+	/// its default (or pass 0 explicitly) this object is useless until
+	/// you vivify it with operator =() or assign().
+	RefCountedPointer(T* c = 0) :
+	counted_(c),
+	refs_(c ? new size_t(1) : 0)
+	{
+	}
+
+	/// \brief Copy constructor
+	RefCountedPointer(const ThisType& other) :
+	counted_(0),		// yes, this is required! see assign()
+	refs_(0)
+	{
+		assign(other);
+	}
+
+	/// \brief Destructor
+	///
+	/// This only destroys the managed memory if the reference count
+	/// drops to 0.
+	~RefCountedPointer()
+	{
+		detach();
+	}
+
+	/// \brief Sets (or resets) the pointer to the counted object.
+	///
+	/// Before we do the actual assignment, we decrement the previous
+	/// managed object's refcount.  If it falls to 0, it's destroyed.
+	///
+	/// If the refcount is >= 2 when we are called, the other users of
+	/// the managed memory will see no difference; the internal refcount
+	/// will just be 1 lower, which is imperceptible from the outside.
+	/// Only this object sees a change in what's being pointed to.
+	void assign(T* c)
+	{
+		detach();
+
+		if (c) {
+			counted_ = c;
+			refs_ = new size_t(1);
+		}
+		else {
+			counted_ = 0;
+			refs_ = 0;
+		}
+	}
+
+	/// \brief Copy an existing refcounted pointer
+	///
+	/// This just copies the pointer to the underlying counted pointer
+	/// and increases the reference count.
+	///
+	/// If this object already pointed to managed memory, we decrement
+	/// the refcount for it first, and destroy the managed memory block
+	/// if the refcount falls to 0.
+	void assign(const ThisType& other)
+	{
+		detach();
+		
+		if (other.counted_ && other.refs_) {
+			counted_ = other.counted_;
+			refs_ = other.refs_;
+			++(*refs_);
+		}
+		else {
+			counted_ = 0;
+			refs_ = 0;
+		}
+	}
+
+	/// \brief Set (or reset) the pointer to the counted object
+	///
+	/// This is essentially the same thing as assign(T*).  The
+	/// choice between the two is just a matter of syntactic preference.
+	ThisType& operator =(T* c)
+	{
+		assign(c);
+		return *this;
+	}
+
+	/// \brief Copy an existing refcounted pointer
+	///
+	/// This is essentially the same thing as assign(const ThisType&).
+	/// The choice between the two is just a matter of syntactic
+	/// preference.
+	ThisType& operator =(const ThisType& rhs)
+	{
+		assign(rhs);
+		return *this;
+	}
+
+	/// \brief Access the object through the smart pointer
+	T* operator ->() const
+	{
+		return counted_;
+	}	
+
+	/// \brief Dereference the smart pointer
+	T& operator *() const
+	{
+		return *counted_;
+	}	
+
+	/// \brief Returns true if this object can be dereferenced.
+	operator bool() const
+	{
+		return counted_ != 0;
+	}
+
+	/// \brief Returns true if this object cannot be safely dereferenced
+	bool operator !() const
+	{
+		return counted_ == 0;
+	}
+
+private:
+	/// \brief Detach ourselves from the managed memory block.
+	///
+	/// If we are managing memory, decreases the reference count and
+	/// destroys the memory if the counter falls to 0.  We don't zero
+	/// out the pointers because none of our callers need it.
+	void detach()
+	{
+		if (refs_ && (--(*refs_) == 0)) {
+			delete counted_;
+			delete refs_;
+		}
+	}
+	
+	/// \brief Pointer to the reference-counted object
+	T* counted_;
+
+	/// \brief Pointer to the reference count.
+	///
+	/// We can't keep this as a plain integer because this object
+	/// allows itself to be copied.  All copies need to share this
+	/// reference count, not just the pointer to the counted object.
+	size_t* refs_;
+};
+
+#endif // !defined(MYSQLPP_REFCOUNTED_H)
+

lib/result.cpp
@@ -43,23 +43,17 @@ ResUse::ResUse(MYSQL_RES* result, Connection* c, bool te) :
 OptionalExceptions(te),
 conn_(c),
 initialized_(false),
-names_(0),
 types_(0),
 fields_(this)
 {
-	if (!result) {
-		result_ = 0;
-		types_ = 0;
-		names_ = 0;
-		return;
+	if ((result_ = result) != 0) {
+		names_ = new FieldNames(this);
+		if (names_) {
+			types_ = new FieldTypes(this);
+		}
+		table_ = fields(0).table;
+		initialized_ = true;
 	}
-	result_ = result;
-	names_ = new FieldNames(this);
-	if (names_) {
-		types_ = new FieldTypes(this);
-	}
-	table_ = fields(0).table;
-	initialized_ = true;
 }
 
 
@@ -78,35 +72,29 @@ ResUse::copy(const ResUse& other)
 
 	set_exceptions(other.throw_exceptions());
 
-	if (!other.result_) {
+	if (other.result_) {
+		result_ = other.result_;
+		fields_ = Fields(this);
+		names_ = other.names_;
+		
+		if (other.types_) {
+			types_ = new FieldTypes(*other.types_);
+		}
+		else {
+			types_ = 0;
+		}
+
+		table_ = other.table_;
+
+		conn_ = other.conn_;
+		initialized_ = true;
+	}
+	else {
 		result_ = 0;
 		types_ = 0;
 		names_ = 0;
 		initialized_ = other.initialized_;
-		return;
 	}
-
-	result_ = other.result_;
-	fields_ = Fields(this);
-
-	if (other.names_) {
-		names_ = new FieldNames(*other.names_);
-	}
-	else {
-		names_ = 0;
-	}
-	
-	if (other.types_) {
-		types_ = new FieldTypes(*other.types_);
-	}
-	else {
-		types_ = 0;
-	}
-
-	table_ = other.table_;
-
-	conn_ = other.conn_;
-	initialized_ = true;
 }
 
 
@@ -132,7 +120,7 @@ ResUse::field_name(int i)
 	if (!names_) {
 		names_ = new FieldNames(this);
 	}
-	return (*names_)[i];
+	return names_->at(i);
 }
 
 
@@ -142,35 +130,33 @@ ResUse::field_name(int i) const
 	if (!names_) {
 		names_ = new FieldNames(this);
 	}
-	return (*names_)[i];
+	return names_->at(i);
 }
 
 
-FieldNames&
+RefCountedPointer<FieldNames>
 ResUse::field_names()
 {
 	if (!names_) {
 		names_ = new FieldNames(this);
 	}
-	return *names_;
+	return names_;
 }
 
 
-const FieldNames&
+const RefCountedPointer<FieldNames>
 ResUse::field_names() const
 {
 	if (!names_) {
 		names_ = new FieldNames(this);
 	}
-	return *names_;
+	return names_;
 }
 
 
 void
 ResUse::reset_field_names()
 {
-	delete names_;
-	names_ = 0;
 	names_ = new FieldNames(this);
 }
 

lib/result.h
@@ -35,6 +35,7 @@
 #include "field_names.h"
 #include "field_types.h"
 #include "noexceptions.h"
+#include "refcounted.h"
 #include "row.h"
 #include "subiter.h"
 
@@ -68,7 +69,6 @@ public:
 	conn_(0),
 	result_(0),
 	initialized_(false),
-	names_(0),
 	types_(0),
 	fields_(this)
 	{
@@ -167,7 +167,6 @@ public:
 			result_ = 0;
 		}
 
-		delete names_;
 		names_ = 0;
 
 		delete types_;
@@ -228,10 +227,10 @@ public:
 	const std::string& field_name(int) const;
 
 	/// \brief Get the names of the fields within this result set.
-	FieldNames& field_names();
+	RefCountedPointer<FieldNames> field_names();
 
 	/// \brief Get the names of the fields within this result set.
-	const FieldNames& field_names() const;
+	const RefCountedPointer<FieldNames> field_names() const;
 
 	/// \brief Reset the names in the field list to their original
 	/// values.
@@ -264,10 +263,11 @@ public:
 	const std::string& names(int i) const { return field_name(i); }
 
 	/// \brief Alias for field_names()
-	FieldNames& names() { return field_names(); }
+	RefCountedPointer<FieldNames> names() { return field_names(); }
 
 	/// \brief Alias for field_names()
-	const FieldNames& names() const { return field_names(); }
+	const RefCountedPointer<FieldNames> names() const
+			{ return field_names(); }
 
 	/// \brief Alias for reset_field_names()
 	void reset_names() { reset_field_names(); }
@@ -314,10 +314,12 @@ protected:
 	Connection* conn_;			///< server result set comes from
 	mutable MYSQL_RES* result_;	///< underlying C API result set
 	bool initialized_;			///< if true, object is fully initted
-	mutable FieldNames* names_;	///< list of field names in result
 	mutable FieldTypes* types_;	///< list of field types in result
 	Fields fields_;				///< list of fields in result
 	std::string table_;			///< table result set comes from
+
+	/// \brief list of field names in result
+	mutable RefCountedPointer<FieldNames> names_;
 
 	/// \brief Copy another ResUse object's contents into this one.
 	///

commit 40381f40fbb686836c2b78df557d16f906d82c76

Fixed a silly regression made in r1751 that causes GCC 4.1 to barf on lib/row.h
git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1774 5946f024-35f4-0310-b1b3-9a3be3380cfb

lib/row.h
@@ -44,6 +44,7 @@ namespace mysqlpp {
 #if !defined(DOXYGEN_IGNORE)
 // Make Doxygen ignore this
 class FieldNames;
+class MYSQLPP_EXPORT ResUse;
 #endif
 
 /// \brief Manages rows from a result set.