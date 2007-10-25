992c861d3413b10d62ef9bb0476733db04d8be63
wyoung <wyoung@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2007-10-25 18:27:54 +0000

    Whitespace fix
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1792 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- examples/cmdline.cpp
+++ examples/cmdline.cpp
@@ -52,65 +52,65 @@ int ag_optind = 1;
 static int
 att_getopt(int argc, char* const argv[], const char* ag_opts)
 {
-    static int optopt;
-    static int sp = 1;
-    register int c;
-    register const char *cp;
+	static int optopt;
+	static int sp = 1;
+	register int c;
+	register const char *cp;
 
-    if (sp == 1) {
-        /* If all args are processed, finish */
-        if (ag_optind >= argc) {
-            return EOF;
-        }
-        if (argv[ag_optind][0] != '-' || argv[ag_optind][1] == '\0') {
-            return EOF;
-        }
-    }
-    else if (!strcmp(argv[ag_optind], "--")) {
-        /* No more ag_options to be processed after this one */
-        ag_optind++;
-        return EOF;
-    }
+	if (sp == 1) {
+		/* If all args are processed, finish */
+		if (ag_optind >= argc) {
+			return EOF;
+		}
+		if (argv[ag_optind][0] != '-' || argv[ag_optind][1] == '\0') {
+			return EOF;
+		}
+	}
+	else if (!strcmp(argv[ag_optind], "--")) {
+		/* No more ag_options to be processed after this one */
+		ag_optind++;
+		return EOF;
+	}
 
-    optopt = c = argv[ag_optind][sp];
+	optopt = c = argv[ag_optind][sp];
 
-    /* Check for invalid ag_option */
-    if (c == ':' || (cp = strchr(ag_opts, c)) == NULL) {
-        fprintf(stderr, "%s: illegal option -- %c\n", argv[0], c);
-        if (argv[ag_optind][++sp] == '\0') {
-            ag_optind++;
-            sp = 1;
-        }
+	/* Check for invalid ag_option */
+	if (c == ':' || (cp = strchr(ag_opts, c)) == NULL) {
+		fprintf(stderr, "%s: illegal option -- %c\n", argv[0], c);
+		if (argv[ag_optind][++sp] == '\0') {
+			ag_optind++;
+			sp = 1;
+		}
 
-        return '?';
-    }
+		return '?';
+	}
 
-    /* Does this ag_option require an argument? */
-    if (*++cp == ':') {
-        /* If so, get argument; if none provided output error */
-        if (argv[ag_optind][sp + 1] != '\0') {
-            ag_optarg = &argv[ag_optind++][sp + 1];
-        }
-        else if (++ag_optind >= argc) {
-            fprintf(stderr,
-                    "%s: option requires an argument -- %c\n", argv[0], c);
-            sp = 1;
-            return '?';
-        }
-        else {
-            ag_optarg = argv[ag_optind++];
-        }
-        sp = 1;
-    }
-    else {
-        if (argv[ag_optind][++sp] == '\0') {
-            sp = 1;
-            ag_optind++;
-        }
-        ag_optarg = NULL;
-    }
+	/* Does this ag_option require an argument? */
+	if (*++cp == ':') {
+		/* If so, get argument; if none provided output error */
+		if (argv[ag_optind][sp + 1] != '\0') {
+			ag_optarg = &argv[ag_optind++][sp + 1];
+		}
+		else if (++ag_optind >= argc) {
+			fprintf(stderr,
+					"%s: option requires an argument -- %c\n", argv[0], c);
+			sp = 1;
+			return '?';
+		}
+		else {
+			ag_optarg = argv[ag_optind++];
+		}
+		sp = 1;
+	}
+	else {
+		if (argv[ag_optind][++sp] == '\0') {
+			sp = 1;
+			ag_optind++;
+		}
+		ag_optarg = NULL;
+	}
 
-    return c;
+	return c;
 }
 
 
@@ -146,28 +146,28 @@ parse_command_line(int argc, char *argv[], const char** ppdb,
         const char** ppserver, const char** ppuser, const char** pppass,
         const char* extra_parms)
 {
-    if (argc < 1) {
-        std::cerr << "Bad argument count: " << argc << '!' << std::endl;
-        return false;
-    }
+	if (argc < 1) {
+		std::cerr << "Bad argument count: " << argc << '!' << std::endl;
+		return false;
+	}
 
-    if (ppdb && !*ppdb) {
-        *ppdb = "mysql_cpp_data";       // use default DB
-    }
+	if (ppdb && !*ppdb) {
+		*ppdb = "mysql_cpp_data";       // use default DB
+	}
 
-    int ch;
-    while ((ch = att_getopt(argc, argv, "m:p:s:u:D")) != EOF) {
-        switch (ch) {
+	int ch;
+	while ((ch = att_getopt(argc, argv, "m:p:s:u:D")) != EOF) {
+		switch (ch) {
 			case 'm': run_mode = atoi(ag_optarg); break;
-            case 'p': *pppass = ag_optarg;        break;
-            case 's': *ppserver = ag_optarg;      break;
-            case 'u': *ppuser = ag_optarg;        break;
-            case 'D': dtest_mode = true;          break;
-            default:
-                print_usage(argv[0], extra_parms);
-                return false;
-        }
-    }
+			case 'p': *pppass = ag_optarg;        break;
+			case 's': *ppserver = ag_optarg;      break;
+			case 'u': *ppuser = ag_optarg;        break;
+			case 'D': dtest_mode = true;          break;
+			default:
+				print_usage(argv[0], extra_parms);
+				return false;
+		}
+	}
 
 	return true;
 }
