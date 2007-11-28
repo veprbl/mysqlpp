f0658e2d06b716abefcea9b7b48bd9cef708bb78
wyoung <wyoung@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2007-11-28 02:35:19 +0000

    Comment change
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@1896 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- lib/result.cpp
+++ lib/result.cpp
@@ -1,10 +1,10 @@
 /***********************************************************************
  result.cpp - Implements the Result, ResNSel, and ResUse classes.
 
- Copyright (c) 1998 by Kevin Atkinson, (c) 1999, 2000 and 2001 by
- MySQL AB, and (c) 2004, 2005 by Educational Technology Resources, Inc.
- Others may also hold copyrights on code in this file.  See the CREDITS
- file in the top directory of the distribution for details.
+ Copyright (c) 1998 by Kevin Atkinson, (c) 1999-2001 by MySQL AB, and
+ (c) 2004-2007 by Educational Technology Resources, Inc.  Others may
+ also hold copyrights on code in this file.  See the CREDITS file in the
+ top directory of the distribution for details.
 
  This file is part of MySQL++.
 
