7e7849024ae7f371279a7c077079be1e2f357f68
mysqlpp <mysqlpp@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2007-12-29 03:07:26 +0000

    Small tweaks to stock.h
    git-svn-id: svn://svn.gna.org/svn/mysqlpp/trunk@2052 5946f024-35f4-0310-b1b3-9a3be3380cfb

--- examples/stock.h
+++ examples/stock.h
@@ -27,14 +27,12 @@
 #include <mysql++.h>
 #include <custom.h>
 
-#include <string>
-
 // The following is calling a very complex macro which will create
 // "struct stock", which has the member variables:
 //
 //   sql_char item;
 //   ...
-//   sql_string description;
+//   Null<sql_mediumtext> description;
 //
 // plus methods to help populate the class from a MySQL row.  See the
 // SSQLS sections in the user manual for further details.
